3b2a4015f79fec900a419399e68b82f1942aad17
Stephani Zatta <64806920+stephanizatta@users.noreply.github.com>
2025-02-06 15:00:36 -0300

    Add files

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task !== '') {
+      setTasks([...tasks, { id: Date.now(), text: task, completed: false }]);
+      setTask('');
+    }
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  const toggleTaskCompletion = (id) => {
+    setTasks(tasks.map(task =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    ));
+  };
+
+  return (
+    <div className="App">
+      <h1>Tasks List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Task"
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+
+      <ul className="task-list">
+        {tasks.map((task) => (
+          <li key={task.id} className={task.completed ? 'completed' : ''}>
+            <span onClick={() => toggleTaskCompletion(task.id)}>
+              {task.text}
+            </span>
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
